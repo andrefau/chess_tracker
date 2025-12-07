e7511c86872dc731acec6af93423b79b24b8e4af
Andreas Fauske <anfauske@gmail.com>
2025-12-07 22:24:08 +0100

    Increased kFactor

--- src/lib/elo.ts
+++ src/lib/elo.ts
@@ -1,4 +1,4 @@
-export function calculateElo(ratingA: number, ratingB: number, actualScoreA: number, kFactor = 32) {
+export function calculateElo(ratingA: number, ratingB: number, actualScoreA: number, kFactor = 96) {
     const expectedScoreA = 1 / (1 + Math.pow(10, (ratingB - ratingA) / 400));
     const newRatingA = ratingA + kFactor * (actualScoreA - expectedScoreA);
     return Math.round(newRatingA);
